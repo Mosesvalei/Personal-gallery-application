8d538a05d5835ac71e9736177c04b9e47dfc35af
Mosesvalei <moses.mwanzia@student.moringaschool.com>
2021-09-14 22:17:01 +0300

    urls links

---
+++ personal/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'personal'
+
+urlpatterns=[
+  url(r'^$', views.home, name='home'),
+  url(r'^search/', views.search_results, name='search'),
+] + static(
+  settings.MEDIA_URL, 
+  document_root = settings.MEDIA_ROOT
+)
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
